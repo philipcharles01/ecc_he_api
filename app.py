8a3207b3e7c61f1b7ced8898a03ddf1e567dc478
baofix <baofix@gmail.com>
2025-07-25 06:00:47 -0700

    FIxed app.py and requirements.txt

--- app.py
+++ app.py
@@ -1,32 +1,7 @@
-from flask import Flask, jsonify
-from Crypto.PublicKey import ECC
-from phe import paillier
+from flask import flask
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "ECC + Homomorphic Encryption API"
-
-@app.route('/generate_keys')
-def generate_keys():
-    ecc_key = ECC.generate(curve='P-256')
-    ecc_private = ecc_key.export_key(format='PEM')
-    ecc_public = ecc_key.public_key().export_key(format='PEM')
-
-    public_key, private_key = paillier.generate_paillier_keypair()
-
-    return jsonify({
-        "ecc_public": ecc_public,
-        "ecc_private": ecc_private,
-        "he_public": {
-            "n": str(public_key.n)
-        },
-        "he_private": {
-            "p": str(private_key.p),
-            "q": str(private_key.q)
-        }
-    })
-
-if __name__ == '__main__':
-    app.run()
+	return "Flask ECC + HE API is working"
